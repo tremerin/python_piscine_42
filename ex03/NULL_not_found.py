a5df4473ffb3bb0d4c128765fd8e8709784df2a0
Tremerin <tremerin@hotmail.com>
2024-12-18 06:21:17 +0100

    ex04

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,7 +1,5 @@
-# var name
+"""
 import inspect
-import math
-
 x, y, z = 1, 2, 3
 
 def retrieve_name(var):
@@ -9,32 +7,36 @@ def retrieve_name(var):
     return [var_name for var_name, var_val in callers_local_vars if var_val is var]
 
 print(retrieve_name(y))
-
+"""
+import math
 
 def NULL_not_found(object: any) -> int:
-    print(type(object))
-    #if isinstance(object, str):
-    #    print("string")
     if object is None:
         print(f"Nothing: None {type(object)}")
     elif type(object) is float:
-        print(f"Chesse: nan {type(object)}")
+        if math.isnan(object):
+            print(f"Chesse: nan {type(object)}")
+    elif object == "":
+        print(f"Empty: {type(object)}")
+    elif type(object) is bool and object is False:
+        print(f"Fake: False {type(object)}")
+    elif type(object) is int and object == 0:
+        print(f"Zero: {type(object)}")
+    else:
+        print("Type not Found")
+        return 1
     return 0
-
-print(type(math.nan))
-print(math.nan)
 
 Nothing = None
 Garlic = float("NaN")
-my_float = 0.1
+my_float = 0.0
 Zero = 0
 Empty = ""
 Fake = False
-print("test_01")
+
 NULL_not_found(Nothing)
 NULL_not_found(Garlic)
-NULL_not_found(my_float)
-#NULL_not_found(Zero)
-#NULL_not_found(Empty)
-#NULL_not_found(Fake)
-#print(NULL_not_found("Brian"))
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Fake)
+print(NULL_not_found("Brian"))
