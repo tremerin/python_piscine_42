6e99d9df71a469ef4a775edcaf27b73c781bd24a
Tremerin <tremerin@hotmail.com>
2024-12-19 03:41:42 +0100

    ex04 ok

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,14 +1,13 @@
 import sys
 
 if len(sys.argv) == 2:
-    #try
-    num = int(sys.argv[1])
-    if type(num) != int:
-        print("AssertionError: argument is not an integer")
-    else:
+    try:
+        num = int(sys.argv[1])
         if num % 2 == 0:
             print("I'm Even.")
         else:
             print("I'm Odd")
+    except:
+        print("AssertionError: argument is not an integer")
 elif len(sys.argv) > 2:
     print("AssertionError: more than one argument is provided")
