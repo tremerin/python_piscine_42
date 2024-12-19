2f0dabdfdf42f5fa8237ce9d09199e6e175a0bbe
tremerin <tremerin13@gmail.com>
2024-12-19 19:48:13 +0100

    ex05 finished

--- ex05/building.py
+++ ex05/building.py
@@ -1,6 +1,18 @@
 import sys
 
+def is_punctuation(char:str):
+    """
+    Checks if the character is a punctuation mark and return True
+    """
+    punctuations_chars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    if char in punctuations_chars:
+        return True
+    return False
+
 def character_counter(string:str):
+    """
+    Analyzes the type of characteres and print them 
+    """
     upper_letters:int = 0
     lower_letters:int = 0
     punctuation_marks:int = 0
@@ -12,6 +24,7 @@ def character_counter(string:str):
         elif element.islower(): lower_letters += 1
         elif element.isspace(): spaces += 1
         elif element.isnumeric(): digits += 1
+        elif is_punctuation(element): punctuation_marks += 1 
 
     print(f"The text contain {characters} characters:")
     print(f"{upper_letters} upper_letters")
@@ -21,6 +34,9 @@ def character_counter(string:str):
     print(f"{digits} digits")
     
 def main():
+    """
+    Receives a text as an argument or waits for input and parses and prints the character type
+    """
     if len(sys.argv) == 1:
         print("What is the text to count?")
         argument = input()
